0a189d175713d1aecd7fbcf12ca0e379d3e318c4
tetreum <basura@yelidmod.com>
2020-03-23 17:36:18 +0100

    + Give permission to runOnBoot.sh too

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo pip install vobject pytz
 sudo chmod +x start.sh
+sudo chmod +x runOnBoot.sh
 printf '{\n"minutesBetweenFetches" : 30,\n"timezone" : "Europe/Madrid",\n"hourFormat" : 12,\n"calendar" : "HTTP_GOOGLE_CALENDAR_SECRET_ADDRESS_HERE"\n}' > ./config.json
